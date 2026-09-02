310821cd7d26fb6e8e595e8067ac437aa0ec666b
swapnaliveer202-hash <swapnaliveer202@gmail.com>
2026-09-02 05:14:43 +0000

    change

--- crcreceiver.cpp
+++ crcreceiver.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-// Function to perform XOR
 string xorOperation(string a, string b)
 {
     string result = "";
@@ -14,28 +12,22 @@ string xorOperation(string a, string b)
         else
             result += '1';
     }
-
     return result;
 }
 
-// Function to calculate CRC
 string calculateCRC(string data, string generator)
 {
     int genLength = generator.length();
-
     string temp = data.substr(0, genLength);
-
     for (int i = genLength; i < data.length(); i++)
     {
         if (temp[0] == '1')
             temp = xorOperation(temp, generator);
         else
             temp = xorOperation(temp, string(genLength, '0'));
-
         temp += data[i];
     }
-
-    // Final XOR
+    
     if (temp[0] == '1')
         temp = xorOperation(temp, generator);
     else
@@ -43,42 +35,26 @@ string calculateCRC(string data, string generator)
 
     return temp;
 }
-
 int main()
 {
     string receivedData, generator;
-
     cout << "========== RECEIVER SIDE ==========\n\n";
-
     cout << "Enter Received Data: ";
     cin >> receivedData;
-
     cout << "Enter Generator: ";
     cin >> generator;
-
-    // Number of extra zero bits
     int extraZeros = generator.length() - 1;
-
-    // Calculate remainder
     string remainder = calculateCRC(receivedData, generator);
-
     cout << "\n----------- OUTPUT -----------\n";
-
     cout << "Received Data         : "
          << receivedData << endl;
-
     cout << "Generator             : "
          << generator << endl;
-
     cout << "Number of Extra 0 Bits: "
          << extraZeros << endl;
-
     cout << "CRC Remainder         : "
          << remainder << endl;
-
-    // Check for error
     bool error = false;
-
     for (char bit : remainder)
     {
         if (bit != '0')
@@ -87,7 +63,6 @@ int main()
             break;
         }
     }
-
     if (error)
     {
         cout << "\nResult: ERROR DETECTED!" << endl;
@@ -98,8 +73,6 @@ int main()
         cout << "\nResult: NO ERROR" << endl;
         cout << "Data received successfully." << endl;
     }
-
     cout << "\n===================================\n";
-
     return 0;
 }
--- crcsender.cpp
+++ crcsender.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-// Function to perform XOR
 string xorOperation(string a, string b)
 {
     string result = "";
@@ -14,11 +12,8 @@ string xorOperation(string a, string b)
         else
             result += '1';
     }
-
     return result;
 }
-
-// Function to calculate CRC
 string calculateCRC(string data, string generator)
 {
     int genLength = generator.length();
@@ -34,58 +29,35 @@ string calculateCRC(string data, string generator)
 
         temp += data[i];
     }
-
-    // Final XOR
     if (temp[0] == '1')
         temp = xorOperation(temp, generator);
     else
         temp = xorOperation(temp, string(genLength, '0'));
-
     return temp;
 }
-
 int main()
 {
     string data, generator;
-
     cout << "========== SENDER SIDE ==========\n\n";
-
     cout << "Enter Data Bits: ";
     cin >> data;
-
     cout << "Enter Generator: ";
     cin >> generator;
-
-    // Number of extra zero bits
     int extraZeros = generator.length() - 1;
-
-    // Add extra zeros
     string dataWithZeros = data + string(extraZeros, '0');
-
-    // Calculate CRC
     string crc = calculateCRC(dataWithZeros, generator);
-
-    // Create transmitted data
     string transmittedData = data + crc;
-
     cout << "\n----------- OUTPUT -----------\n";
-
     cout << "Data Bits              : " << data << endl;
     cout << "Generator              : " << generator << endl;
-
     cout << "Number of Extra 0 Bits : "
          << extraZeros << endl;
-
     cout << "Data + Extra 0 Bits    : "
          << dataWithZeros << endl;
-
     cout << "CRC Remainder          : "
          << crc << endl;
-
     cout << "Transmitted Data       : "
          << transmittedData << endl;
-
     cout << "\n=================================\n";
-
     return 0;
 }
